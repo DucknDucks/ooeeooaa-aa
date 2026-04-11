bccb9b88b07abc5c9e95d38c31b0210612d04a3d
ribbit <mufhussein@gmail.com>
2026-04-11 22:38:08 +0100

    this is the start of my operator empire, aswell as some added functions into my booloop class

--- oopprojlogga.cpp
+++ oopprojlogga.cpp
@@ -14,9 +14,40 @@ using namespace std;
 
 class boolop{
 public:
+    virtual string nameget() const = 0;
+    virtual bool eval(bool a, bool b) const{return false;}
+    virtual string explanget() const = 0;
+    virtual bool noteval(bool a)const{return false;}
+    virtual bool isnot() const{return false;}
+    virtual ~boolop(){}
+};
 
+class AND : public boolop{
 
+};
 
+class OR : public boolop{
 
+};
 
+class XOR : public boolop{
+
+};  
+
+class NOT : public boolop{
+
+};
+
+class NAND : public boolop{
+
+};
+
+class NOR : public boolop{
+
+};
+
+int main(){
+    cout <<"\n><================================================><\n";
+    cout <<"\n               boolean truth table sim              \n";
+    cout <<"\n><================================================><\n";
 }
