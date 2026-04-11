3874b82b574697e35966323417ff290b6073b118
ribbit <mufhussein@gmail.com>
2026-04-11 19:11:58 +0100

    start of the explanation requirement

---
+++ oopprojlogga.cpp
@@ -0,0 +1,22 @@
+/*
+NTO1046
+OOP(object-oriented programming) project
+Author: Hussein Hajee
+Student ID: 40501349
+boolean expression and truth table gen
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class boolop{
+public:
+
+
+
+
+
+}
